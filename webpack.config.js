6526d0f6cc92b5c83d1e214f53930534f3c56434
Hon Lam <hon@junglescout.com>
2018-09-11 01:47:33 -0700

    Add hot reloading.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,7 +7,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   rules: [
     { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-  ]  
+  ]
 };
